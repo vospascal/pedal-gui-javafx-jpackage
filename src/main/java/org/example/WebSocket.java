d21d925e454bfede8ae5cab77575b2ddca6b235a
Pascal Vos <vospascal@gmail.com>
2021-01-20 21:13:19 +0100

    websocket server

---
+++ src/main/java/org/example/WebSocket.java
@@ -0,0 +1,80 @@
+package org.example;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class WebSocket {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+
+        ServerSocket server = new ServerSocket(9000);
+        try {
+            System.out.println("Server has started on localhost:9000.\r\nWaiting for a connection...");
+            Socket client = server.accept();
+            System.out.println("A client connected.");
+
+            InputStream in = client.getInputStream();
+            OutputStream out = client.getOutputStream();
+            Scanner s = new Scanner(in, "UTF-8");
+
+            try {
+                String data = s.useDelimiter("\\r\\n\\r\\n").next();
+                Matcher get = Pattern.compile("^GET").matcher(data);
+                if (get.find()) {
+                    String wsKeyConst = "Sec-WebSocket-Key:";
+                    Matcher match = Pattern.compile(wsKeyConst + " (.*)").matcher(data);
+                    match.find();
+                    String wsKey = match.group(1);
+                    System.out.println("Sec-Websocket-Key: " + wsKey);
+                    byte[] response = ("HTTP/1.1 101 Switching Protocols\r\n"
+                            + "Connection: Upgrade\r\n"
+                            + "Upgrade: websocket\r\n"
+                            + "Sec-WebSocket-Accept: "
+                            + Base64.getEncoder().encodeToString(MessageDigest.getInstance("SHA-1").digest((wsKey + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes("UTF-8")))
+                            + "\r\n\r\n").getBytes("UTF-8");
+                    out.write(response, 0, response.length);
+                }
+
+                DataInputStream inStream = new DataInputStream(in);
+                DataOutputStream outStream = new DataOutputStream(out);
+
+                while (client.isConnected()) {
+                    if (inStream.available() > 0) {
+                        Frame inF = new Frame(inStream);
+                        System.out.println("data:" +  new String(inF.getData(), StandardCharsets.UTF_8));
+
+                        Frame outF = new Frame("test");
+                        outF.send(outStream);
+
+                        outF = new Frame("test", "HUUGE!");
+                        outF.send(outStream);
+
+                    } else {
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+            } finally {
+                s.close();
+            }
+        } finally {
+            server.close();
+        }
+    }
+}
