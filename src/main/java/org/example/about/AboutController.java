b91c56ebaae3ccbe0f88eab7013526782c1fa830
Pascal Vos <vospascal@gmail.com>
2022-06-14 14:43:07 +0200

    add more translations and fixed some encoding

--- src/main/java/org/example/about/AboutController.java
+++ src/main/java/org/example/about/AboutController.java
@@ -1,7 +1,13 @@
 package org.example.about;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import org.example.PrimaryController;
+import org.example.UserStorageAndConfiguration;
 
 import java.awt.*;
 import java.io.IOException;
@@ -9,10 +15,54 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class AboutController {
+
     private PrimaryController controller;
 
     public void injectMainController(PrimaryController primaryController){
         this.controller = primaryController;
+    }
+    @FXML
+    public TextFlow aboutTextFlow;
+
+    public void initialize() {
+        Text textThankyou = new Text();
+        textThankyou.textProperty().bind(UserStorageAndConfiguration.createStringBinding("tab.about.label.thankyou"));
+
+        Text textResources = new Text();
+        textResources.textProperty().bind(UserStorageAndConfiguration.createStringBinding("tab.about.label.resources"));
+
+
+        Text textGuiLabel = new Text();
+        textGuiLabel.textProperty().bind(UserStorageAndConfiguration.createStringBinding("tab.about.label.resources.gui"));
+
+        Hyperlink linkGui = new Hyperlink();
+        linkGui.setOnAction(this::gui);
+        linkGui.textProperty().bind(UserStorageAndConfiguration.createStringBinding("tab.about.link.resources.gui"));
+
+        Text textArduinoLabel = new Text();
+        textArduinoLabel.textProperty().bind(UserStorageAndConfiguration.createStringBinding("tab.about.label.resources.arduino"));
+
+        Hyperlink linkArduino = new Hyperlink();
+        linkArduino.setOnAction(this::arduino);
+        linkArduino.textProperty().bind(UserStorageAndConfiguration.createStringBinding("tab.about.link.resources.arduino"));
+
+
+        Text textDonateLabel = new Text();
+        textDonateLabel.textProperty().bind(UserStorageAndConfiguration.createStringBinding("tab.about.label.donate"));
+
+        Hyperlink donateLink = new Hyperlink();
+        donateLink.setOnAction(this::paypal);
+        donateLink.textProperty().bind(UserStorageAndConfiguration.createStringBinding("tab.about.link.donate"));
+
+
+        ObservableList list = aboutTextFlow.getChildren();
+        list.addAll(
+                textThankyou,  new Text("\n"),
+                textResources,  new Text("\n"),
+                textGuiLabel, linkGui,  new Text("\n"),
+                textArduinoLabel, linkArduino,  new Text("\n"),
+                textDonateLabel, donateLink,  new Text("\n")
+        );
     }
 
     public void gui(ActionEvent actionEvent) {
